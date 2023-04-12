43740fcef1b71d125cbd27484ff2df4aed826a5c
LeonardoGonzalez1717 <127991188+LeonardoGonzalez1717@users.noreply.github.com>
2023-04-12 15:45:27 -0400

    Add files via upload

---
+++ templeat/footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <a href="https://lacandelaria.com.ve/" target="_blank">
+            <h3>U.E.P A.P.E.P "La Candelaria" | 2021-2023</h3>
+
+        </a>
+        <h4>Made by &copy; Leonardo</h4>
+    </footer>
+    
+
+    <script src="https://kit.fontawesome.com/5818af7131.js" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ templeat/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="http://localhost/crud_yere/styles.css">
+    <title>Document</title>
+          <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<!-- Iconos cdn-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+<body>
+
+
+
+    <header>
+        <h1>U.E.P A.P.E.P "La Candelaria"</h1>
+    </header>
+
+
+
